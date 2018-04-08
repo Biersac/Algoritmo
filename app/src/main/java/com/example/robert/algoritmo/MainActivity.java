fb4226e3cd914f7c7ba81a245dc235cb48fe7dca
Robert <uncavolo@gmail.com>
2018-04-08 10:28:57 +0200

    Stable version.

--- app/src/main/java/com/example/robert/algoritmo/MainActivity.java
+++ app/src/main/java/com/example/robert/algoritmo/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.robert.algoritmo;
 
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.widget.Button;
 import android.widget.EditText;
 import android.content.Intent;
 import android.os.Bundle;
@@ -26,6 +29,11 @@ public class MainActivity extends AppCompatActivity {
     private static final String stringUUID = "00001101-0000-1000-8000-00805F9B34FB";
     private static final String TAG = "MainActivity";
     private static final int REQUEST_ENABLE_BT = 1;
+    private static final double ANGLE_PER_UNIT = 5.628;
+    private static final double ANGLE_MAX = 70.35;
+    private static final double ANGLE_MIN = -70.35;
+    private static final int NUMBER_OF_UNITS = 25;
+    private static final double ANGLE_OFFSET = - 70.35;
 
     private static UUID myUUID;
 
@@ -35,9 +43,14 @@ public class MainActivity extends AppCompatActivity {
     private EditText editTextBluetoothAddress;
     private ImageView imageViewPointerROLL;
     private ImageView imageViewPointerPITCH;
+    private ImageView imageViewIndicator;
+    private ImageView imageViewIndicatorPointer;
+    private Button buttonSwitchActivity;
 
     Pattern pattern_string;
     Pattern pattern_values;
+
+
 
 
     public class ThreadConnect extends Thread {
@@ -122,7 +135,14 @@ public class MainActivity extends AppCompatActivity {
                         String[] values = group.split(";");
                         final float ROLL = Float.parseFloat(values[10]);
                         final float PITCH = Float.parseFloat(values[11]);
-                        Log.i(TAG, "ROLL: " + ROLL + ", PITCH: " + PITCH);
+                        final int AOA = Integer.parseInt(values[17]);
+                        Log.i(TAG, "ROLL: " + ROLL + ", PITCH: " + PITCH + ", Angle of Attack: " + AOA);
+
+
+                        float RESULT = (float) ((AOA + 5) * ANGLE_PER_UNIT + ANGLE_OFFSET);
+                        Log.i(TAG, "Result: " + RESULT);
+                        imageViewIndicatorPointer.setRotation(-RESULT);
+                            // -RESULT perché è in senso orario.
                             // pitch e roll hanno valori in gradi, che vanno da -90 a +90.
                             // gruppi 10 e 11 per ROLL e PITCH
                         imageViewPointerROLL.setRotation(ROLL);
@@ -151,6 +171,9 @@ public class MainActivity extends AppCompatActivity {
         this.editTextBluetoothAddress = findViewById(R.id.editTextBluetoothAddress);
         this.imageViewPointerPITCH = findViewById(R.id.imageViewPointerPITCH);
         this.imageViewPointerROLL = findViewById(R.id.imageViewPointerROLL);
+        this.imageViewIndicator = findViewById(R.id.imageViewIndicator);
+        this.imageViewIndicatorPointer = findViewById(R.id.imageViewIndicatorPointer);
+        this.buttonSwitchActivity = findViewById(R.id.buttonSwitchActivity);
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
         if (this.bluetoothAdapter != null) {
@@ -181,5 +204,9 @@ public class MainActivity extends AppCompatActivity {
         BluetoothDevice bluetoothDevice = bluetoothAdapter.getRemoteDevice(address);
         threadConnect = new ThreadConnect(bluetoothDevice);
         threadConnect.start();
+    }
+
+    public void buttonSwitchActivityOnClick(View view) {
+        startActivity(new Intent(getApplicationContext(), SecondActivity.class));
     }
 }
---
+++ app/src/main/java/com/example/robert/algoritmo/SecondActivity.java
@@ -0,0 +1,31 @@
+package com.example.robert.algoritmo;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private static final String TAG = "SecondActivity";
+
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+
+
+
+    }
+
+
+
+}
+
+
